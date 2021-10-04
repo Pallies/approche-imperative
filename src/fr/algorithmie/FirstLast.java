7471cbc3d6d8c1a002dc240c48d8cc2d75cdb3f1
Pallies <palliescie@gmail.com>
2021-10-04 22:58:30 +0200

    firstLast

---
+++ src/fr/algorithmie/FirstLast.java
@@ -0,0 +1,19 @@
+package fr.algorithmie;
+
+public class FirstLast {
+	static int[] array1 = {1, 15, -3, 8, 2, 3, 0, 14, -4} ;
+	static int[] array2 = {3, 15, -3, 8, 7, 4, 3} ;
+	static int[] array3 = {1, 15, -3, 8, 0, 14, 6} ;
+	static int[] array4 = {} ;
+	public static void main(String[] args) {
+		System.out.println(" reponse : "+ controlFirstLast6(array1));
+		System.out.println(" reponse : "+ controlFirstLast6(array2));
+		System.out.println(" reponse : "+ controlFirstLast6(array3));
+		System.out.println(" reponse : "+ controlFirstLast6(array4));
+
+	}
+	public static boolean controlFirstLast6(int[] array) {
+		return array.length > 0 && array[0] ==  array[array.length-1];
+	}
+
+}
