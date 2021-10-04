76e4d99abb62cba5c3d2576eb647172f9276bf3f
Pallies <palliescie@gmail.com>
2021-10-04 21:38:34 +0200

    rechercheMax

--- src/fr/algorithmie/AffichagePartiel.java
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -5,7 +5,9 @@ public class AffichagePartiel {
 	public static void main(String[] args) {
 		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
 		afficheSuperieurA3(array);
-
+		affichePair(array);
+		afficheIndexPair(array);
+		afficheImpair(array);
 	}
 
 	/**
@@ -26,13 +28,42 @@ public class AffichagePartiel {
 	/**
 	 * affiche les nombres du tableau 
 	 * Si ils sont pairs
-	 * 
-	 * @param array
+	 * @param int[] array
 	 */
 	public static void affichePair(int[] array) {
 		int indexArray = 0;
 		while (indexArray < array.length) {
 			if (array[indexArray] % 2 == 0) {
+				System.out.println(array[indexArray]);
+			}
+			indexArray++;
+		}
+	}
+	
+	/**
+	 * affiche les nombre du tableau
+	 * si ils ont un index pair
+	 * @param int[] array
+	 */
+	public static void afficheIndexPair(int[] array) {
+		int indexArray=0;
+		while(indexArray< array.length) {
+			if(indexArray%2==0) {
+				System.out.println(array[indexArray]);
+			}
+			
+			indexArray++;
+		}
+	}
+	/**
+	 * affiche les nombres du tableau
+	 * Si ils sont impairs
+	 * @param int[] array
+	 */
+	public static void afficheImpair(int[] array) {
+		int indexArray=0; 
+		while(indexArray< array.length) {
+			if(array[indexArray]% 2 == 1) {
 				System.out.println(array[indexArray]);
 			}
 			indexArray++;
---
+++ src/fr/algorithmie/RechercheMax.java
@@ -0,0 +1,44 @@
+package fr.algorithmie;
+
+import java.util.ArrayList;
+
+public class RechercheMax {
+	static int[]  array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+	static ArrayList<Integer> arrayInteger = new ArrayList<Integer>();
+
+	public static void main(String[] args) {
+         int max = convertArrayForMax();
+         int max2 = afficheMax();
+         
+         System.out.println("method Stream "+max);
+         System.out.println("method array "+max2);
+
+	}
+
+	/**
+	 * recherchela valeur la plus grande
+	 * avec un stream().max
+	 * @return int max
+	 */
+	public static Integer convertArrayForMax() {
+		int indexArray = 0;
+		while (indexArray < array.length) {
+			arrayInteger.add(array[indexArray]);
+			indexArray++;
+		}
+		
+		return arrayInteger.stream().max(Integer::compare).get();
+	}
+
+	public static int afficheMax() {
+		int indexArray=0;
+		int max=array[0];
+		while(indexArray<array.length) {
+			if(array[indexArray]>max) {
+				max = array[indexArray];
+			}
+			indexArray++;
+		}
+		return max;
+	}
+}
