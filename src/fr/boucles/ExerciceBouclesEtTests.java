9cc0c7b93bc56bbe980aade3e534a2b0c626f309
Pallies <palliescie@gmail.com>
2021-10-04 16:10:29 +0200

    Exercice tab entier

---
+++ src/fr/boucles/ExerciceBouclesEtTests.java
@@ -0,0 +1,10 @@
+package fr.boucles;
+
+public class ExerciceBouclesEtTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -3,21 +3,21 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-       
-         byte varByte=1;
-          short varShort=237;
-          int varInt=178;
-          long varLong=67L;
-          float varFloat=2F;
-         double varDouble=67;
-          char varChar='a';
-          boolean varBoolean=false;
+
+//         byte varByte=1;
+//          short varShort=237;
+//          int varInt=178;
+//          long varLong=67L;
+//          float varFloat=2F;
+//         double varDouble=67;
+//          char varChar='a';
+//          boolean varBoolean=false;
           String varString="java";
 
-        System.out.println(" cours" +varString);
+        System.out.println("cours " +varString);
         String randomString = "Voici le résultat d'un calcul : 1+5=6";
 
-        int indexSeparate = randomString.lastIndexOf(':');
+        int indexSeparate = randomString.indexOf(':');
         
         String strSlice1= randomString.substring(0, indexSeparate+1)+"\n";
         String strSlice2 = randomString.substring(indexSeparate+1);
