ed25b915f8b806c43cd5aa53d9501739c1f3816f
Pallies <palliescie@gmail.com>
2021-10-05 09:52:50 +0200

    interactif chiffres suivants

--- src/fr/algorithmie/InteractifChiffresSuivants.java
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -1,12 +1,31 @@
 package fr.algorithmie;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InteractifChiffresSuivants {
 
 	static Scanner scanner;
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		int nb = 0;
+		while (true) {
+			try {
+				scanner = new Scanner(System.in);
+				System.out.println(" Veuillez entrer un chiffre.");
+				nb = scanner.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+
+				System.out.println("votre caractère \" " + scanner.next() + " \" n'est pas un nombre");
+			}
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.print(nb + i);
+			if (i != 9) {
+				System.out.print(",");
+			}
+		}
 
 	}
 
